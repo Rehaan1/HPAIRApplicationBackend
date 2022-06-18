378344b5e4b959b59a043f250998e89508a09e28
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-06-18 21:54:53 +0530

    feat: project setup

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        status: 200,
+        message: "HPAIR Application Backend Up and Running. Refer Documentation"
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on Port ' + process.env.PORT)
+  })
