978cb9c615ff840c74191d58679dcb554fa4f9f7
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-07-02 22:36:32 +0530

    fix: firebase config secret fix

--- firebase/fb.js
+++ firebase/fb.js
@@ -2,5 +2,5 @@ const admin = require('firebase-admin')
 
 // Initializing Firebase Admin
 module.exports = admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii')))
 })
