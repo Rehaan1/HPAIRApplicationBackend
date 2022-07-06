cbcf3022476d75c6e600e2d2f07bd66d63e7971c
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-07-06 16:39:26 +0530

    feat: update article done

--- api/routes/articleRoute.js
+++ api/routes/articleRoute.js
@@ -80,4 +80,91 @@ router.post('/create', verifyToken, upload.single('file'), async (req, res) => {
     })
 })
 
+router.patch('/update', verifyToken, upload.single('file'), async (req, res) => {
+  if (!req.body.title) {
+    return res.status(400).json({
+      error: 'missing required parameter. refer documentation'
+    })
+  }
+
+  if (!req.body.docId) {
+    return res.status(400).json({
+      error: 'missing required parameter. refer documentation'
+    })
+  }
+
+  if (!req.body.content) {
+    return res.status(400).json({
+      error: 'missing required parameter. refer documentation'
+    })
+  }
+
+  if (!req.body.featured) {
+    return res.status(400).json({
+      error: 'missing required parameter. refer documentation'
+    })
+  }
+
+  if (req.file) {
+    let name = saltedMd5(req.file.originalname, process.env.SALT)
+    name = name + uuid4()
+    const fileName = name + path.extname(req.file.originalname)
+
+    await bucket.file(fileName)
+      .createWriteStream()
+      .end(req.file.buffer)
+      .on('finish', (data) => {
+        const imageUrl = process.env.BASE_URL + fileName
+
+        articlesRef.doc(req.body.docId).update({
+          title: req.body.title,
+          content: req.body.content,
+          featured: req.body.featured,
+          imageUrl: imageUrl
+        })
+          .then((data) => {
+            return res.status(200).json({
+              status: '200',
+              message: 'Article Created Successfully',
+              articleId: data
+            })
+          })
+          .catch((error) => {
+            console.log(error)
+            return res.status(400).json({
+              success: false,
+              err: error
+            })
+          })
+      })
+      .on('error', (err) => {
+        console.log(err)
+        return res.status(400).json({
+          status: 400,
+          val: err
+        })
+      })
+  } else {
+    articlesRef.doc(req.body.docId).update({
+      title: req.body.title,
+      content: req.body.content,
+      featured: req.body.featured
+    })
+      .then((data) => {
+        return res.status(200).json({
+          status: '200',
+          message: 'Article Updated Successfully',
+          articleId: data
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+        return res.status(400).json({
+          success: false,
+          err: error
+        })
+      })
+  }
+})
+
 module.exports = router
